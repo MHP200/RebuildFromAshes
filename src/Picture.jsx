d4c7e50e3121fceeab0cccec0deb2864a185cf29
ethanlunderville <ethanjlunderville@gmail.com>
2023-05-23 21:25:32 -0700

    update

---
+++ src/Picture.jsx
@@ -0,0 +1,9 @@
+import React from 'react' 
+
+function Picture(path) {
+  return (
+    <div>Picture</div>
+  )
+}
+
+export default Picture
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import '../public/index.css'
+import '../public/fun.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+)
